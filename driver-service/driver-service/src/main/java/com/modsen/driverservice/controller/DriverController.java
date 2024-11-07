4d785eca5921b2307891ac9de16bffd59839cc37
artyomshpl <47445048+artyomshpl@users.noreply.github.com>
2024-11-07 15:55:35 +0300

    fix endpoints, add pagination, move logic to service layer

--- driver-service/driver-service/src/main/java/com/modsen/driverservice/controller/DriverController.java
+++ driver-service/driver-service/src/main/java/com/modsen/driverservice/controller/DriverController.java
@@ -1,45 +1,35 @@
 package com.modsen.driverservice.controller;
 
 import com.modsen.driverservice.dto.DriverDTO;
-import com.modsen.driverservice.exception.DriverNotFoundException;
-import com.modsen.driverservice.mapper.DriverMapper;
-import com.modsen.driverservice.model.Driver;
-import com.modsen.driverservice.service.impl.DriverService;
-import lombok.AllArgsConstructor;
+import com.modsen.driverservice.service.DriverService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 @RestController
-@RequestMapping("/drivers")
-@AllArgsConstructor
+@RequestMapping("/api/v1/drivers")
+@RequiredArgsConstructor
 public class DriverController {
     private final DriverService driverService;
-    private final DriverMapper driverMapper;
 
     @GetMapping
-    public List<DriverDTO> getAllDrivers() {
-        return driverService.getAllDrivers().stream()
-                .map(driverMapper::toDTO)
-                .collect(Collectors.toList());
+    public Page<DriverDTO> getAllDrivers(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return driverService.getAllDrivers(pageable);
     }
 
     @GetMapping("/{id}")
     public DriverDTO getDriverById(@PathVariable String id) {
-        Optional<Driver> driver = driverService.getDriverById(id);
-        if (driver.isPresent()) {
-            return driverMapper.toDTO(driver.get());
-        } else {
-            throw new DriverNotFoundException("Driver not found with id: " + id);
-        }
+        return driverService.getDriverById(id);
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public DriverDTO saveDriver(@RequestBody DriverDTO driverDTO) {
-        Driver driver = driverMapper.toEntity(driverDTO);
-        return driverMapper.toDTO(driverService.saveDriver(driver));
+        return driverService.saveDriver(driverDTO);
     }
 
     @DeleteMapping("/{id}")
