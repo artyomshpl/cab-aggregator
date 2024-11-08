ec092bcbcf43248068c3bd000c54a8263358c4ad
artyomshpl <47445048+artyomshpl@users.noreply.github.com>
2024-11-08 16:05:16 +0300

    fix endpoints, fix service, add ResponseEntity wrapper

--- driver-service/driver-service/src/main/java/com/modsen/driverservice/exception/DriverNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.modsen.driverservice.exception;
-
-public class DriverNotFoundException extends RuntimeException {
-    public DriverNotFoundException(String message) {
-        super(message);
-    }
-}
--- driver-service/driver-service/src/main/java/com/modsen/driverservice/repository/DriverRepository.java
+++
@@ -1,7 +0,0 @@
-package com.modsen.driverservice.repository;
-
-import com.modsen.driverservice.model.Driver;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface DriverRepository extends MongoRepository<Driver, String> {
-}
