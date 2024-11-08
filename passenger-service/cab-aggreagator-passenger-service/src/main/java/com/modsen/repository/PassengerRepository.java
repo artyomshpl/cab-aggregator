fffb7142eccf8f1c541746be727d6bed5110bcda
artyomshpl <47445048+artyomshpl@users.noreply.github.com>
2024-11-08 18:59:59 +0300

    move logic to service, dto(class -> records), add pages, small fixes

--- passenger-service/cab-aggreagator-passenger-service/src/main/java/com/modsen/repository/PassengerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.modsen.repository;
-
-import com.modsen.model.Passenger;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PassengerRepository extends JpaRepository<Passenger, Long> {
-    Passenger findByName(String firstName);
-}
