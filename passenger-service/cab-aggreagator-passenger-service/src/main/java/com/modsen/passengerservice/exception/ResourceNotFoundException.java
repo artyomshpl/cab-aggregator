19f264d245192f2ccd73c89847114c8a8e92e3cb
artyomshpl <47445048+artyomshpl@users.noreply.github.com>
2024-11-11 16:37:44 +0300

    fixing branches

--- passenger-service/cab-aggreagator-passenger-service/src/main/java/com/modsen/passengerservice/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.modsen.passengerservice.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
