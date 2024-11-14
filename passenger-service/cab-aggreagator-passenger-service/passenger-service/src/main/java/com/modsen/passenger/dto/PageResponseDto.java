4a510cf032501c5b9c9ff44974303ccd167861f2
artyomshpl <47445048+artyomshpl@users.noreply.github.com>
2024-11-14 17:42:33 +0300

    fix PageResponseDto

--- passenger-service/cab-aggreagator-passenger-service/passenger-service/src/main/java/com/modsen/passenger/dto/PageResponseDto.java
+++ passenger-service/cab-aggreagator-passenger-service/passenger-service/src/main/java/com/modsen/passenger/dto/PageResponseDto.java
@@ -2,4 +2,9 @@ package com.modsen.passenger.dto;
 
 import java.util.List;
 
-public record PageResponseDto<T>(List<T> content, int totalPages, long totalElements) {}
+public record PageResponseDto<T>(
+        List<T> content,
+        int totalPages,
+        long totalElements
+) {
+}
