f1d9e530240f227fe06d301eb5ae8f5a6d1767c4
artyomshpl <47445048+artyomshpl@users.noreply.github.com>
2024-12-19 16:16:59 +0300

    remove not used code in kafka producer

--- rides-service/rides-service/src/main/java/com/modsen/rides/kafka/impl/KafkaProducerImpl.java
+++ rides-service/rides-service/src/main/java/com/modsen/rides/kafka/impl/KafkaProducerImpl.java
@@ -2,19 +2,15 @@ package com.modsen.rides.kafka.impl;
 
 import com.modsen.rides.dto.DriverDto;
 import com.modsen.rides.dto.PassengerDto;
-import com.modsen.rides.dto.RideDto;
 import com.modsen.rides.kafka.KafkaProducer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 public class KafkaProducerImpl implements KafkaProducer {
     private final KafkaTemplate<String, Object> kafkaTemplate;
-    private final KafkaTemplate<String, List<RideDto>> KafkaTemplateList;
 
     @Override
     public void sendNeedFreeDrivers(String message) {
@@ -29,9 +25,5 @@ public class KafkaProducerImpl implements KafkaProducer {
     @Override
     public void sendPassengerUpdates(PassengerDto message) {
         kafkaTemplate.send("passenger-updates", message);
-    }
-
-    public void sendRides(List<RideDto> message) {
-        KafkaTemplateList.send("send-rides", message);
     }
 }
