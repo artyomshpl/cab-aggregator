5e1d48cc8f03bb34dc5040e7a2265d959c5aaccc
artyomshpl <47445048+artyomshpl@users.noreply.github.com>
2025-01-28 16:48:57 +0300

    fix endpoint

--- notification-service/src/main/java/com/modsen/notification/controller/NotificationController.java
+++ notification-service/src/main/java/com/modsen/notification/controller/NotificationController.java
@@ -3,17 +3,19 @@ package com.modsen.notification.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import com.modsen.notification.service.NotificationService;
 
 @RestController
+@RequestMapping("api/v1/notifications")
 @RequiredArgsConstructor
 public class NotificationController {
 
     private final NotificationService notificationService;
 
-    @GetMapping("/notifications/{driverId}")
+    @GetMapping("/{driverId}")
     public SseEmitter subscribe(@PathVariable String driverId) {
         return notificationService.subscribe(driverId);
     }
